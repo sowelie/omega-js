3ee318fef7ed335a12a989419ad8579e0d9a94e7
kpinette <sowelie@gmail.com>
2015-01-26 22:33:43 -0500

    Added a basic grunt build to create finalized output files.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,7 @@
 module.exports = function(grunt) {
 
+    require('load-grunt-tasks')(grunt);
+
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
@@ -10,10 +12,54 @@ module.exports = function(grunt) {
                 jshintrc: ".jshintrc",
                 reporter: require('jshint-summary')
             }
+        },
+
+        watch: {
+            files: ['<%= jshint.files %>'],
+            tasks: ['jshint']
+        },
+
+        clean: ["dist"],
+
+        requirejs: {
+            options: {
+                preserveLicenseComments: false,
+                optimize: "uglify"
+            },
+
+            omega: {
+                options: {
+                    baseUrl: "src",
+                    mainConfigFile: 'src/omega/config.release.js',
+                    include: ["omega/main"],
+                    exclude: ["jquery", "text"],
+                    out: "dist/js/omega.min.js"
+                }
+            }
+        },
+
+        cssmin: {
+            dist: {
+                files: {
+                    "dist/css/omega.min.css": "css/omega.css",
+                    "dist/css/jquery-ui-1.10.0.custom.min.css": "css/jquery-ui-1.10.0.custom.css"
+                }
+            }
+        },
+
+        copy: {
+            dist: {
+                files: [
+                    { expand: true, src: ["images/*"], dest: "dist/" },
+                    { expand: true, src: ["LICENSE.md", "README.md" ], dest: "dist/" },
+                    { expand: true, src: ['bower_components/jquery/jquery.min.js'], dest: "dist/" }
+                ]
+            }
         }
     });
 
     // Default task(s).
     grunt.registerTask('default', ['jshint']);
+    grunt.registerTask('dist', ['clean', 'requirejs', 'cssmin:dist', 'copy']);
 
 };
--- src/omega/config.release.js
+++ src/omega/config.release.js
@@ -6,6 +6,8 @@ require.config({
 	// 3rd party script alias names (Easier to type "jquery" than "libs/jquery-1.8.2.min")
 	paths: {
 
+		"jquery": "empty:",
+		"text": "../bower_components/requirejs-text/text"
 
 	}
 
